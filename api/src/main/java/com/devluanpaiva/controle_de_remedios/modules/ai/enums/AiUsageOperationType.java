83ed59251aab5035625c57a6133b368803c5222a
Luan Alves <devluanpaiva@gmail.com>
2026-07-28 13:34:34 -0300

    feat(api): creating ai enums: - AiUsageContentType - AiUsageOperationType

---
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/ai/enums/AiUsageOperationType.java
@@ -0,0 +1,12 @@
+package com.devluanpaiva.controle_de_remedios.modules.ai.enums;
+
+public enum AiUsageOperationType {
+
+    ESUS_PRESCRIPTION_EXTRACTION,
+
+    DIGITALIZED_PRESCRIPTION_EXTRACTION,
+
+    BARCODE_EXTRACTION,
+
+    MEDICINE_NAME_EXTRACTION;
+}
