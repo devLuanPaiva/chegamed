45f79481d9a6faa5f8977a0bb9efba35d12f2a57
Luan Alves <devluanpaiva@gmail.com>
2026-07-30 07:07:58 -0300

    feat(api): implementing  recordUsage method

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/assistant/service/AssistantService.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/assistant/service/AssistantService.java
@@ -2,7 +2,10 @@ package com.devluanpaiva.controle_de_remedios.modules.assistant.service;
 
 import com.devluanpaiva.controle_de_remedios.modules.assistant.dto.ChatRequestDTO;
 import com.devluanpaiva.controle_de_remedios.modules.assistant.dto.ChatResponseDTO;
+import com.devluanpaiva.controle_de_remedios.modules.assistant.dto.RecordAssistantUsageRequestDTO;
 
 public interface AssistantService {
     ChatResponseDTO chat(ChatRequestDTO dto);
+
+    void recordUsage(RecordAssistantUsageRequestDTO dto);
 }
