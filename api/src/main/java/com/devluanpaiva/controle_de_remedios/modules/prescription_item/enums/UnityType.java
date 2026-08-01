9577b191be11621d25c3f80c377aebf802b9784c
Luan Alves <devluanpaiva@gmail.com>
2026-08-01 19:48:13 -0300

    refactor(api): changing UnityType values

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/prescription_item/enums/UnityType.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/prescription_item/enums/UnityType.java
@@ -1,10 +1,7 @@
 package com.devluanpaiva.controle_de_remedios.modules.prescription_item.enums;
 
 public enum UnityType {
-    BOX,
-    BLISTER,
     BOTTLE,
-    TABLET,
-    CAPSULE,
-    SYRUP,
+    CREAM,
+    LIQUID,
 }
