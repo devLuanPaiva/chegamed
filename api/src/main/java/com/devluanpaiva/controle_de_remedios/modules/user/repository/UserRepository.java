51e91c9d2d8582652dd836bc56cfeaace807627f
Luan Alves <devluanpaiva@gmail.com>
2026-08-12 11:17:10 -0300

    fix(api): implementing findByIdwithCompanies method

--- api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/user/repository/UserRepository.java
+++ api/src/main/java/com/devluanpaiva/controle_de_remedios/modules/user/repository/UserRepository.java
@@ -7,13 +7,20 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devluanpaiva.controle_de_remedios.modules.user.entity.User;
 import com.devluanpaiva.controle_de_remedios.modules.user.enums.UserRole;
 
 public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+    @EntityGraph(attributePaths = "companies")
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByIdWithCompanies(@Param("id") UUID id);
+
     Optional<User> findByEmail(String email);
 
     Optional<User> findByEmailIgnoreCase(String email);
